702aada4a07600d0a6d22a78f61c3a59b8703ea4
Christophe Guthoerl <contact@webtransform.fr>
2024-11-05 19:40:23 +0100

    symfony7 header+footer+variable+boucle

--- src/Controller/RandomImageController.php
+++ src/Controller/RandomImageController.php
@@ -11,8 +11,16 @@ class RandomImageController extends AbstractController
     #[Route('/random/image', name: 'app_random_image')]
     public function index(): Response
     {
+        /* Création d'une variable pour stocker les images aléatoire */
+        $images = [];
+
+        for ($i = 0; $i < 9; $i++) {
+            $images[] = 'https://picsum.photos/300/200?random=' . $i;
+        }
+
         return $this->render('random_image/index.html.twig', [
-            'controller_name' => 'RandomImageController',
+            'images' => $images,
+            
         ]);
     }
 }
